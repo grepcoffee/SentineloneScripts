f32faac6ad0da72a80ca9f911f9256ffa2cd87dd
jeff <jeff@jmatt.io>
2019-10-22 16:49:54 -0400

    added s1 pull user and asset script

---
+++ GetAssetandLastLoggedinUser/GetAssetandLastLoggedinUser.py
@@ -0,0 +1,54 @@
+import requests
+import json 
+import os
+
+# Import Enviroment variable
+APITOKEN = os.environ.get('S1APITOKEN')
+S1BASEURL = os.environ.get('S1BASETOKEN')
+S1APIURL = os.environ.get('S1APIURL')
+
+#Make API Call
+url = S1APIURL
+querystring = {"apiToken":APITOKEN}
+headers = {
+    'Accept': "*/*",
+    'Host': S1BASEURL,
+    'Accept-Encoding': "gzip, deflate",
+    'Connection': "keep-alive",
+    'cache-control': "no-cache"
+    }
+
+# Response parsing
+response = requests.request("GET", url, headers=headers, params=querystring)
+responsejson = json.loads(response.text)
+responsedata = (responsejson['data'])
+
+# Open file for writing
+openfile = open('S1Export.csv','w')
+
+# Print Initial Entry and export to file
+for assetid in responsedata:     
+    addata = assetid['activeDirectory']
+    compname = (assetid['computerName'])   
+    lastuser = (assetid['lastLoggedInUserName'])
+    s1data = (compname +","+ lastuser)
+    openfile.write((s1data)+'\n')
+
+# Print additional entries and paginate
+cursor = (responsejson['pagination'])['nextCursor']
+while cursor != None:
+    cururl = url + "&cursor=" + cursor
+    response = requests.request("GET", cururl, headers=headers, params=querystring)
+    responsejson = json.loads(response.text)
+    responsedata = (responsejson['data'])
+    cursor = (responsejson['pagination'])['nextCursor']
+
+    for assetid in responsedata:     
+        addata = assetid['activeDirectory']
+        compname = (assetid['computerName'])   
+        lastuser = (assetid['lastLoggedInUserName'])
+        s1data = (compname +","+ lastuser)
+        openfile.write((s1data)+'\n')
+
+# Close File
+openfile.close()
---
+++ GetAssetandLastLoggedinUser/README.md
@@ -0,0 +1,8 @@
+# S1 Asset and User Pull Script
+
+This script pulls all assets enrolled in S1 along with the last logged in user. 
+
+# Required Variables
+
+* APITOKEN: S1 API Token which can be generated from the console. 
+* S1BASEURL: S1 Baseurl for your org. Typically this will be https://yourorg.sentinelone.net/
